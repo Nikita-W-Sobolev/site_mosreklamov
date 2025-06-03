c75642cc138a7c4bbd68326c7b933fad72b4cf7b
Nikita Sobolev <nikitasobmois1@gmail.com>
2025-06-03 23:13:47 +0300

    Added a basic template

--- mosreklamov/main_app/views.py
+++ mosreklamov/main_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.template.loader import render_to_string
 
-# Create your views here.
+def index(request):
+    data = {
+        'title': 'Mosreklamov',
+    }
+    return render(request, "main_app/index.html", context=data)
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1><u>Страница не найдена</u></h1>')
--- mosreklamov/mosreklamov/urls.py
+++ mosreklamov/mosreklamov/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from main_app import views
+from main_app.views import page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main_app.urls')),
 ]
+
+
+handler404 = page_not_found
